692da04107a72cafa47f6a870a32a422f334c0ab
JHarvin <jeffethramos@gmail.com>
2018-08-25 23:06:04 -0600

    poniendole nombre al sistema

--- Vistas/menuVentas.php
+++ Vistas/menuVentas.php
@@ -1,5 +1,5 @@
 <!-- Navbar-->
-    <header class="app-header" style="background-color:#8D8D8D;"><a class="app-header__logo" href="inicio.php" style="background-color:#E84D13;">Baterias Chacon</a>
+    <header class="app-header" style="background-color:#8D8D8D;"><a class="app-header__logo" href="inicio.php" style="background-color:#E84D13;">rentalSys(Baterias)</a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
