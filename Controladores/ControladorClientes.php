b146feac2fc425f47912c457a86488ed0d795417
JHarvin <jeffethramos@gmail.com>
2018-09-13 14:22:31 -0600

    corregido lo de saul

--- Controladores/ControladorClientes.php
+++ Controladores/ControladorClientes.php
@@ -13,21 +13,19 @@ class ClientesController{
     
     #FUNCIÓN REGISTRAR
     public function registrarCliente(){
-        $genero="";
         
         #VALIDACION DE LOS DATOS--------
         if(isset($_POST["nombre"]) && !empty($_POST["nombre"]) && isset($_POST["telefono"]) && !empty($_POST["telefono"]) && isset($_POST["dui"]) && !empty($_POST["dui"]) &&
-        isset($_POST["licencia"]) && !empty($_POST["licencia"]) &&
-        isset($_POST["direccion"]) && !empty($_POST["direccion"]) &&
-        isset($_POST["sexo"]) && !empty($_POST["sexo"])                                      
+          isset($_POST["licencia"]) && !empty($_POST["licencia"]) &&
+           isset($_POST["direccion"]) && !empty($_POST["direccion"] &&
+        isset($_POST["sexo"]) && !empty($_POST["sexo"])                                      )
           ){
+            
             # validacion que todo este correcto es decir caracteres y cantidad de estos lado servidor
             # preg_match realiza una comparacion de expresiones regulares
-
             if(preg_match('/^[a-zA-Z]+$/',$_POST["nombre"]) && preg_match('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/',$_POST["email"]) &&
               preg_match('/^[0-9]+$/',$_POST["telefono"])
               ){}
-
             if($_POST["sexo"]=="Masculino" ){
                 $genero="M";
             }
@@ -38,18 +36,17 @@ class ClientesController{
         $datosClienteController =array("nombre"=>strtoupper($_POST["nombre"]), "telefono"=>$_POST["telefono"], 
         "dui"=>$_POST["dui"],
         "licencia"=>$_POST["licencia"],
+        "direccion"=>$_POST["direccion"],
         "genero"=>$genero);
         
-       
-           
-    
-           $respuesta=DatosCliente::registroClienteModel($datosClienteController,"tclientes");
+        
+        $respuesta=DatosCliente::registroClienteModel($datosClienteController,"tclientes");
         
         if( $respuesta=="success"){
             echo' 
              
             <script type="text/javascript">
-              
+           
 
           alertify.success("Registro Guardado    ✔");
 
@@ -65,9 +62,9 @@ class ClientesController{
                echo' 
              
             <script type="text/javascript">
-              
+             alertify.set("notifier","position", "top-center");
 
-          alertify.error("EL DUI O LICENCIA YA HAN SIDO REGISTRADOS PRUEBA OTRA ");
+          alertify.error("Algo salio mal :(");
 
 
 
@@ -81,8 +78,8 @@ class ClientesController{
         
         
         
-        
         }
+        
     
         
     }
@@ -113,8 +110,8 @@ class ClientesController{
                   
                   <td>
                   <div class="btn-group" role="group">
-                  <a href="actualizarDatosUsuario.php?id='.$item["dui"].'" id="btnEditar" name="btnEditar" class="btn btn-info"   ><i class="fa fa-edit"></i></a>
-                  <a href="usuarios.php?idb='.$item["dui"].'" class="btn btn-danger" onclick=""><i class="fa fa-trash-o"></i></a>
+                  <a href="#" id="btnEditar" name="btnEditar" class="btn btn-info"   ><i class="fa fa-edit"></i></a>
+                  <a href="#" class="btn btn-danger" onclick=""><i class="fa fa-trash-o"></i></a>
                   </div>
                   </td>
                  
--- Controladores/ControladorRegistrarProveedor.php
+++ Controladores/ControladorRegistrarProveedor.php
@@ -71,8 +71,11 @@ require_once"../Modelos/ModeloProveedores.php";
                   <td>'.$item["direccion"].'</td>
                  
                   
-                  <td><a href="actualizarDatosUsuario.php?id='.$item["idproveedor"].'" id="btnEditar" name="btnEditar" class="btn btn-info"   ><i class="fa fa-edit"></i></a>
+                  <td>
+                  <div class="btn-group" role="group">
+                  <a href="actualizarDatosUsuario.php?id='.$item["idproveedor"].'" id="btnEditar" name="btnEditar" class="btn btn-info"   ><i class="fa fa-edit"></i></a>
                   <a href="usuarios.php?idb='.$item["idproveedor"].'" class="btn btn-danger" onclick=""><i class="fa fa-trash-o"></i></a>
+                  </div>
                   </td>
                  
                 </tr>
