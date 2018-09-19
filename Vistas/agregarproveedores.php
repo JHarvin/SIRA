87a238838f55843946d72137299e21c663e13beb
Yancifany <yancifany@gmail.com>
2018-09-19 12:55:44 -0600

    ultimo baterias 2

--- Vistas/agregarproveedores.php
+++ Vistas/agregarproveedores.php
@@ -7,7 +7,7 @@ require_once"../Controladores/ControladorRegistrarProveedor.php";
 <html lang="es">
 <head>
 
-    <title>Proveedores</title>
+    <title>Agregar Proveedores</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
