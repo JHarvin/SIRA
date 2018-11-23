34bced09998d7038ebd32628c1546c8ab411b864
JHarvin <jeffethramos@gmail.com>
2018-11-23 13:23:22 -0600

    post

--- Vistas/generarVentas.php
+++ Vistas/generarVentas.php
@@ -33,7 +33,7 @@ $datos=$lista->Baterias();
         </ul>
  </div>
 <div class="row" >
-      <form class="POST">
+      <form method="POST">
        <div  class="col-md-12">
            <div class="card col-md-3" style="float:right;">
                <div class="card-tittle" style="background-color:#E84D13;">
